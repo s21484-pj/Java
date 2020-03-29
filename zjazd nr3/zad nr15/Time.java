b9f19338d331a3e4199011652450a0e5be1def71
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-03-29 17:56:23 +0200

    Create Time.java

---
+++ zjazd nr3/zad nr15/Time.java
@@ -0,0 +1,78 @@
+/*
+    Time UML chart
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public class Time {
+
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public void setTime(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public String toString() {
+        return String.format("%02d", hour) + ":" + String.format("%02d", minute) + ":" + String.format("%02d", second);
+    }
+
+    public Time nextSecond() {
+        if (second == 59) {
+            second = 0;
+            if (minute == 59) {
+                minute = 0;
+                if (hour == 23) {
+                    hour = 0;
+                } else hour += 1;
+            } else minute += 1;
+        } else second += 1;
+        return this;
+    }
+
+    public Time previousSecond() {
+        if (second == 0) {
+            second = 59;
+            if (minute == 0) {
+                minute = 59;
+                if (hour == 0) {
+                    hour = 23;
+                } else hour -= 1;
+            } else minute -= 1;
+        } else second -= 1;
+        return this;
+    }
+}
