c6386ef6182dddf7fc223d320bc9a95fb7a2e49c
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-03-29 17:55:10 +0200

    Create TestDate.java

---
+++ zjazd nr3/zad nr14/TestDate.java
@@ -0,0 +1,14 @@
+/*
+    TestDate
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public class TestDate {
+
+    public static void main(String[] args) {
+
+        Date date = new Date(11, 06, 1996);
+        System.out.println(date.toString());
+    }
+}
