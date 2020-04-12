3461656c5929f5825138a373ffd98b76ce9e99c2
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-04-12 22:23:10 +0200

    Create Rectangle.java

---
+++ zjazd nr4/zad nr23/Rectangle.java
@@ -0,0 +1,63 @@
+/*
+    UML Rectangle class
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public class Rectangle extends Shape {
+
+    private double width;
+    private double length;
+
+    public Rectangle() {
+        super();
+        width = 1.0;
+        length = 1.0;
+    }
+
+    public Rectangle(double width, double length) {
+        super();
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(double width, double length, String color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+
+    public double getPerimeter() {
+        return 2 * width + 2 * length;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle[Shape[" +
+                "color=" + getColor() +
+                ", filled=" + isFilled() +
+                "], width=" + width +
+                ", length=" + length +
+                "]";
+    }
+}
