d2db9ab90172135022da6aaabaee373bed862ca8
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-05-29 20:07:20 +0200

    Create Map.java

---
+++ zjazd nr6/zad nr5/Map.java
@@ -0,0 +1,13 @@
+/*
+    Map class
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public class Map implements Turner {
+
+    @Override
+    public void turn() {
+        System.out.println("Turn the map");
+    }
+}
