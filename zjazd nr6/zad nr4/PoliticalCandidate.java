84ff031ce9c0d60611ac09e6c022249468b00086
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-05-29 20:03:48 +0200

    Create PoliticalCandidate.java

---
+++ zjazd nr6/zad nr4/PoliticalCandidate.java
@@ -0,0 +1,13 @@
+/*
+    PoliticalCandidate class
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public class PoliticalCandidate implements Runner {
+
+    @Override
+    public void run() {
+        System.out.println("A political candidate is (not) running to help people.");
+    }
+}
