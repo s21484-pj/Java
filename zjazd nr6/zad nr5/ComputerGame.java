a25dab6f0af2748e83686b1d21bc7a99352d092e
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-05-29 20:07:45 +0200

    Create ComputerGame.java

---
+++ zjazd nr6/zad nr5/ComputerGame.java
@@ -0,0 +1,13 @@
+/*
+    ComputerGame class
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public class ComputerGame implements Turner {
+
+    @Override
+    public void turn() {
+        System.out.println("Now it is your turn");
+    }
+}
