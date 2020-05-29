0ced1c81b54aa3e81c5f3cb7b7c4a2e64f966858
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-05-29 20:03:06 +0200

    Create Athlete.java

---
+++ zjazd nr6/zad nr4/Athlete.java
@@ -0,0 +1,13 @@
+/*
+    Athlete class
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public class Athlete implements Runner {
+
+    @Override
+    public void run() {
+        System.out.println("Athletes run everyday.");
+    }
+}
