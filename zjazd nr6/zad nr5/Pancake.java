15ab1957b4278047810a90d9d5902c489f408744
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-05-29 20:06:38 +0200

    Create Pancake.java

---
+++ zjazd nr6/zad nr5/Pancake.java
@@ -0,0 +1,13 @@
+/*
+    Pancake class
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public class Pancake implements Turner {
+
+    @Override
+    public void turn() {
+        System.out.println("Flipping");
+    }
+}
