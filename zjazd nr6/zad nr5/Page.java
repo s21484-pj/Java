27c26df62a433229c257e5d85cf42cb5aa3e8b07
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-05-29 20:06:19 +0200

    Create Page.java

---
+++ zjazd nr6/zad nr5/Page.java
@@ -0,0 +1,13 @@
+/*
+    Page class
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public class Page implements Turner {
+
+    @Override
+    public void turn() {
+        System.out.println("Going to the next page");
+    }
+}
