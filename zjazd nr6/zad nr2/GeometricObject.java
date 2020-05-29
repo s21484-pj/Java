73eb34a51894ffa0d787ad7c03175f3df01e2af2
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-05-29 19:54:39 +0200

    Create GeometricObject.java

---
+++ zjazd nr6/zad nr2/GeometricObject.java
@@ -0,0 +1,11 @@
+/*
+    GeometricObject interface
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public interface GeometricObject {
+
+    public double getPerimeter();
+    public double getArea();
+}
