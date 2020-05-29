aab5fd8ddea9f9f62d2d70e0eb303da33c0fc1e8
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-05-29 19:50:39 +0200

    Create Movable.java

---
+++ zjazd nr6/zad nr1/Movable.java
@@ -0,0 +1,16 @@
+/*
+    Interface Movable
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public interface Movable {
+
+    public void moveUp();
+
+    public void moveDown();
+
+    public void moveLeft();
+
+    public void moveRight();
+}
