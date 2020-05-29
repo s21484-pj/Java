86d3f4ce33106a9a51c610f8e65928443f711cee
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-05-29 20:10:53 +0200

    Create LoanConstants.java

---
+++ zjazd nr6/zad nr6/LoanConstants.java
@@ -0,0 +1,14 @@
+/*
+    LoanConstants interface
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public interface LoanConstants {
+
+    public final int shortTerm = 1;
+    public final int mediumTerm = 3;
+    public final int longTerm = 5;
+    public final String company = "Sanchez Construction Loan Co";
+    public final int maxAmount = 100000;
+}
