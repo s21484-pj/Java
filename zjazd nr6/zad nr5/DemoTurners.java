a15d573390924e71156a3a2941066ca23b91a212
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-05-29 20:07:00 +0200

    Create DemoTurners.java

---
+++ zjazd nr6/zad nr5/DemoTurners.java
@@ -0,0 +1,19 @@
+/*
+    DemoTurners class
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public class DemoTurners {
+
+    public static void main(String[] args) {
+
+        Leaf leaf = new Leaf();
+        Page page = new Page();
+        Pancake pancake = new Pancake();
+
+        leaf.turn();
+        page.turn();
+        pancake.turn();
+    }
+}
