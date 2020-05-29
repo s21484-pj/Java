b27b57956defa791ae37bd2e109645cf2779000b
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-05-29 19:55:13 +0200

    Create Circle.java

---
+++ zjazd nr6/zad nr2/Circle.java
@@ -0,0 +1,31 @@
+/*
+    Circle class
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public class Circle implements GeometricObject {
+
+    protected double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle[" +
+                "radius=" + radius +
+                "]";
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
