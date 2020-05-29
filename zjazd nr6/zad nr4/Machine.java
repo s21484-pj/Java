caf6b28e77a12dfa5d1063b32e4752cdaa3487c0
s21484-pj <56191731+s21484-pj@users.noreply.github.com>
2020-05-29 20:02:41 +0200

    Create Machine.java

---
+++ zjazd nr6/zad nr4/Machine.java
@@ -0,0 +1,13 @@
+/*
+    Machine class
+    Author: Maciej Leciejewski
+ */
+package com.company;
+
+public class Machine implements Runner {
+
+    @Override
+    public void run() {
+        System.out.println("The virtual machine is running. We can test other operation system.");
+    }
+}
